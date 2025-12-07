25c27b62d8181c910fb45ba9d59551c848e00fc8
Rohan Godha <git@rohangodha.com>
2025-12-06 20:09:58 -0500

    api work

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use std::hash::Hash;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -11,4 +12,56 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn siebren(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Player {
+    PlayerA,
+    PlayerB,
+}
+#[derive(Clone, Copy, PartialEq)]
+enum TerminalState {
+    Win(Player),
+    Draw,
+}
+
+/// an environment implements a game, like Connect4, that we want to train a model to play.
+///
+/// these environments should be able to rollback state updates to step in and out of states
+/// efficiently, with low copying costs.
+trait Environment: Clone + Hash {
+    /// sent to the neural network evaluator
+    type Observation;
+    /// this should be a cheap type, like an enum/usize. It should impl AsRef<usize>,
+    /// where the usize should be unique and in the range 0<=n<=NUM_ACTIONS;
+    type Action: Copy + Eq + Hash + AsRef<usize>;
+    /// used to rollback a move
+    type RollbackState;
+    const NUM_ACTIONS: usize;
+
+    /// creates an environment. this should be ~randomly generated if possible on every call, to
+    /// avoid the network overfitting on a single map.
+    fn new() -> Self;
+    /// returns None if the game is still going, and Some(PlayerAWin/PlayerBWin/Draw) if its over.
+    fn is_terminal(&self) -> Option<TerminalState>;
+
+    /// returns an iterator over the valid actions. playing an
+    /// invalid action means the current player loses.
+    fn valid_actions(&self) -> impl Iterator<Item = Self::Action>;
+    /// current_player
+    fn current_player(&self) -> Player;
+    /// generates the observation that is fed to the neural network
+    fn observation(&self) -> Self::Observation;
+
+    /// modifies the environment to accept this action. this returns a rollback state, which can be
+    /// passed to the rollback function, such that:
+    ///
+    /// E == rollback(apply_action(E, a))
+    ///
+    /// implementors of this function may assume that the action is a valid action, as defined by
+    /// the valid_actions function.
+    fn apply_action(&mut self, action: Self::Action) -> Self::RollbackState;
+
+    /// rollback function (look at docs for apply_action)
+    fn rollback(&mut self, rollback: Self::RollbackState);
 }
